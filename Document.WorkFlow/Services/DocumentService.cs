5edce2155bd07d4825fbba20c6e6f341f85e8adb
Moses Nero Vodounou <mvodounou@outlook.com>
2022-06-28 00:45:58 +0100

    fixed data nullation but DB not saving changes

--- Document.WorkFlow/Services/DocumentService.cs
+++ Document.WorkFlow/Services/DocumentService.cs
@@ -23,9 +23,6 @@ namespace Document.WorkFlow.Services
                 throw new Exception("File path(s) cannot be empty");
 
             IEnumerable<R>? tempDataCollection = null;
-
-            //var gb = Path.GetExtension(filePaths[0]);
-
             try
             {
                 filePaths.ForEach(filePath =>
@@ -56,43 +53,6 @@ namespace Document.WorkFlow.Services
             }
         }
 
-        public IEnumerable<R> ExtractData2<R>(List<string> filePaths)
-        {
-            if (filePaths is null || !filePaths.Any())
-                throw new Exception("File path(s) cannot be empty");
-
-            IEnumerable<R>? tempDataCollection = null;
-
-            try
-            {
-                filePaths.ForEach(filePath =>
-                {
-                    if (!Path.GetExtension(filePath).Equals(".csv"))
-                    {
-                        throw new Exception("File type not supported.");
-                    }
-
-                    var reader = new StreamReader(path: filePath);
-
-                    var config = new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture) { BadDataFound = null };
-                    var csvDochelper = new CsvReader(reader, config);
-
-                    tempDataCollection = csvDochelper.GetRecords<R>();
-
-                });
-
-                return tempDataCollection;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-            finally
-            {
-                //tempDataCollection = null;
-            }
-        }
-
         public void TransformData<I, O>(IEnumerable<I>? InDataCollection, out IEnumerable<O>? OutDataCollection)
         {
             throw new NotImplementedException();
@@ -100,39 +60,76 @@ namespace Document.WorkFlow.Services
 
         public async Task<bool> LoadpurchaseOrderData(List<PurchaseOrder> DataCollection)
         {
-            if (DataCollection is null || !DataCollection.Any())
-                return false;
+            try
+            {
+                var _tempDataCollection = new List<PurchaseOrder>(DataCollection);
+                if (_tempDataCollection is null || !_tempDataCollection.Any())
+                    return false;
 
-            _dbContext.PurchaseOrders.AddRange(DataCollection);
-            return _dbContext.SaveChanges() <= 0 ? false : true;
+                await _dbContext.PurchaseOrders.AddRangeAsync(DataCollection);
+                _tempDataCollection = null;
+                return _dbContext.SaveChanges() <= 0 ? false : true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
 
         public async Task<bool> LoadpurchaseOrderLineData(List<PurchaseOrderLine> DataCollection)
         {
-            if (DataCollection is null || !DataCollection.Any())
-                return false;
+            try
+            {
+                var _tempDataCollection = new List<PurchaseOrderLine>(DataCollection);
+                if (_tempDataCollection is null || !_tempDataCollection.Any())
+                    return false;
 
-            _dbContext.PurchaseOrderLines.AddRange(DataCollection);
-            return _dbContext.SaveChanges() <= 0 ? false : true;
+                await _dbContext.PurchaseOrderLines.AddRangeAsync(DataCollection);
+                _tempDataCollection = null;
+                return _dbContext.SaveChanges() <= 0 ? false : true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
 
         public async Task<bool> LoadpurchaseOrderSupplierData(List<Supplier> DataCollection)
         {
-            if (DataCollection is null || !DataCollection.Any())
-                return false;
+            try
+            {
 
-            _dbContext.Suppliers.AddRange(DataCollection);
-            return _dbContext.SaveChanges() <= 0 ? false : true;
+                var _tempDataCollection = new List<Supplier>(DataCollection);
+                if (_tempDataCollection is null || !_tempDataCollection.Any())
+                    return false;
+
+                await _dbContext.Suppliers.AddRangeAsync(DataCollection);
+                _tempDataCollection = null;
+                return _dbContext.SaveChanges() <= 0 ? false : true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
 
         public async Task<bool> LoadpurchaseOrderSupplierData(IEnumerable<Supplier> DataCollection)
         {
-            if (DataCollection is null || !DataCollection.Any())
-                return false;
+            try
+            {
+                var _tempDataCollection = new List<Supplier>(DataCollection);
+                if (_tempDataCollection is null || !_tempDataCollection.Any())
+                    return false;
 
-            _dbContext.Suppliers.AddRange(DataCollection);
-            return _dbContext.SaveChanges() <= 0 ? false : true;
+                await _dbContext.Suppliers.AddRangeAsync(DataCollection);
+                _tempDataCollection = null;
+                var res = _dbContext.SaveChanges();
+                return res <= 0 ? false : true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
     }
 }
-
